57a65f081fbdef51c187515aec69fd0a324533c4
Alex Gusev <alex@flancer64.com>
2021-03-10 11:36:29 +0200

    MF-24 Backup and restore commands for live re-deployment

--- src/Cli/Start.mjs
+++ src/Cli/Start.mjs
@@ -1,64 +1,79 @@
+/**
+ * @namespace TeqFw_Http2_Cli_Start
+ */
+// MODULE'S IMPORT
 import $path from 'path';
 import $fs from 'fs';
 
+// DEFINE WORKING VARS
+const NS = 'TeqFw_Http2_Cli_Start';
+
+// DEFINE MODULE'S FUNCTIONS
 /**
- * Factory class to create CLI command to start HTTP2 server.
+ * Factory to create CLI command to start HTTP2 server.
+ *
+ * @param {TeqFw_Di_SpecProxy} spec
+ * @returns {TeqFw_Core_App_Cli_Command_Data}
+ * @constructor
+ * @memberOf TeqFw_Http2_Cli_Start
  */
-export default class TeqFw_Http2_Cli_Start {
+function Factory(spec) {
+    // PARSE INPUT & DEFINE WORKING VARS
+    /** @type {TeqFw_Http2_Defaults} */
+    const DEF = spec['TeqFw_Http2_Defaults$'];   // instance singleton
+    /** @type {TeqFw_Core_App_Launcher.Bootstrap} */
+    const bootCfg = spec[DEF.MOD_CORE.DI_BOOTSTRAP]; // named singleton
+    /** @type {TeqFw_Di_Container} */
+    const container = spec['TeqFw_Di_Container$'];  // instance singleton
+    /** @type {TeqFw_Core_App_Config} */
+    const config = spec['TeqFw_Core_App_Config$'];  // instance singleton
+    /** @type {TeqFw_Core_App_Logger} */
+    const logger = spec['TeqFw_Core_App_Logger$'];  // instance singleton
+    /** @type {typeof TeqFw_Core_App_Cli_Command_Data} */
+    const Command = spec['TeqFw_Core_App_Cli_Command#Data'];    // class constructor
 
-    constructor(spec) {
-        // CONSTRUCTOR INJECTED DEPS
-        /** @type {TeqFw_Http2_Defaults} */
-        const DEF = spec['TeqFw_Http2_Defaults$'];   // instance singleton
-        /** @type {TeqFw_Core_App_Launcher.Bootstrap} */
-        const bootCfg = spec[DEF.MOD_CORE.DI_BOOTSTRAP]; // named singleton
-        /** @type {TeqFw_Di_Container} */
-        const container = spec['TeqFw_Di_Container$'];  // instance singleton
-        /** @type {TeqFw_Core_App_Config} */
-        const config = spec['TeqFw_Core_App_Config$'];  // instance singleton
-        /** @type {TeqFw_Core_App_Logger} */
-        const logger = spec['TeqFw_Core_App_Logger$'];  // instance singleton
-        /** @type {typeof TeqFw_Core_App_Cli_Command_Data} */
-        const Command = spec['TeqFw_Core_App_Cli_Command#Data'];    // class constructor
+    // DEFINE INNER FUNCTIONS
+    /**
+     * Start the HTTP/2 server.
+     * @returns {Promise<void>}
+     * @memberOf TeqFw_Http2_Cli_Start
+     */
+    const action = async function () {
+        logger.info('Starting HTTP/2 server.');
+        /** @type {TeqFw_Http2_Back_Server} */
+        const server = await container.get('TeqFw_Http2_Back_Server$', NS);
+        await server.init();
 
-        // RUNTIME INJECTED DEPS
-        /**  @returns {Promise<TeqFw_Http2_Back_Server>} */
-        const getServer = async () => await container.get('TeqFw_Http2_Back_Server$', this.constructor.name);
+        // collect startup configuration then compose path to PID file
+        const portCfg = config.get('local/server/port');
+        const port = portCfg || DEF.SERVER_DEFAULT_PORT;
+        const pid = process.pid.toString();
+        const pidPath = $path.join(bootCfg.root, DEF.PID_FILE_NAME);
+        // write PID to file then start the server
+        try {
+            $fs.writeFileSync(pidPath, pid);
+            // PID is wrote => start the server
+            await server.listen(port);
+            logger.info(`HTTP/2 server is listening on port ${port}. PID: ${pid}.`);
+        } catch (e) {
+            console.error('%s', e);
+        }
+    };
 
-        // DEFINE THIS INSTANCE METHODS (NOT IN PROTOTYPE)
+    // MAIN FUNCTIONALITY
+    Object.defineProperty(action, 'name', {value: `${NS}.${action.name}`});
 
-        /**
-         * @see TeqFw_Core_App_Cli_Command.create
-         * @returns {Promise<TeqFw_Core_App_Cli_Command>}
-         */
-        this.create = async function () {
-            // this is sample code:
-            const result = new Command();
-            result.ns = DEF.BACK_REALM;
-            result.name = 'start';
-            result.desc = 'Start the HTTP/2 server.';
-            result.action = async function () {
-                logger.info(result.desc);
-                const server = await getServer();
-                await server.init();
-
-                // collect startup configuration then compose path to PID file
-                const portCfg = config.get('local/server/port');
-                const port = portCfg || DEF.SERVER_DEFAULT_PORT;
-                const pid = process.pid.toString();
-                const pidPath = $path.join(bootCfg.root, DEF.PID_FILE_NAME);
-                // write PID to file then start the server
-                try {
-                    $fs.writeFileSync(pidPath, pid);
-                    // PID is wrote => start the server
-                    await server.listen(port);
-                    logger.info(`HTTP2 server is listening on port ${port}. PID: ${pid}.`);
-                } catch (e) {
-                    console.error('%s', e);
-                }
-
-            };
-            return result;
-        };
-    }
+    // COMPOSE RESULT
+    const result = new Command();
+    result.ns = DEF.BACK_REALM;
+    result.name = 'start';
+    result.desc = 'Start the HTTP/2 server.';
+    result.action = action;
+    return result;
 }
+
+// MODULE'S FUNCTIONALITY
+Object.defineProperty(Factory, 'name', {value: `${NS}.${Factory.name}`});
+
+// MODULE'S EXPORT
+export default Factory;
--- src/Cli/Stop.mjs
+++ src/Cli/Stop.mjs
@@ -1,44 +1,63 @@
+/**
+ * @namespace TeqFw_Http2_Cli_Stop
+ */
+// MODULE'S IMPORT
 import $path from 'path';
 import $fs from 'fs';
 
+// DEFINE WORKING VARS
+const NS = 'TeqFw_Http2_Cli_Stop';
+
+// DEFINE MODULE'S FUNCTIONS
 /**
- * Factory class to create CLI command to stop HTTP2 server.
+ * Factory to create CLI command to stop HTTP2 server.
+ *
+ * @param {TeqFw_Di_SpecProxy} spec
+ * @returns {TeqFw_Core_App_Cli_Command_Data}
+ * @constructor
+ * @memberOf TeqFw_Http2_Cli_Stop
  */
-export default class TeqFw_Http2_Cli_Stop {
+function Factory(spec) {
+    // PARSE INPUT & DEFINE WORKING VARS
+    /** @type {TeqFw_Http2_Defaults} */
+    const DEF = spec['TeqFw_Http2_Defaults$'];   // instance singleton
+    /** @type {TeqFw_Core_App_Launcher.Bootstrap} */
+    const bootCfg = spec[DEF.MOD_CORE.DI_BOOTSTRAP]; // named singleton
+    /** @type {typeof TeqFw_Core_App_Cli_Command_Data} */
+    const Command = spec['TeqFw_Core_App_Cli_Command#Data'];    // class constructor
 
-    constructor(spec) {
-        // CONSTRUCTOR INJECTED DEPS
-        /** @type {TeqFw_Http2_Defaults} */
-        const DEF = spec['TeqFw_Http2_Defaults$'];   // instance singleton
-        /** @type {TeqFw_Core_App_Launcher.Bootstrap} */
-        const bootCfg = spec[DEF.MOD_CORE.DI_BOOTSTRAP]; // named singleton
-        /** @type {typeof TeqFw_Core_App_Cli_Command_Data} */
-        const Command = spec['TeqFw_Core_App_Cli_Command#Data'];    // class constructor
+    // DEFINE INNER FUNCTIONS
+    /**
+     * Stop the HTTP/2 server.
+     * @returns {Promise<void>}
+     * @memberOf TeqFw_Http2_Cli_Stop
+     */
+    const action = async function () {
+        try {
+            const pidPath = $path.join(bootCfg.root, DEF.PID_FILE_NAME);
+            const data = $fs.readFileSync(pidPath);
+            const pid = data.toString();
+            console.info(`Stop web server (PID: ${pid}).`);
+            process.kill(pid, 'SIGINT');
+        } catch (e) {
+            console.error('Cannot kill web server process.');
+        }
+    };
 
-        // DEFINE THIS INSTANCE METHODS (NOT IN PROTOTYPE)
+    // MAIN FUNCTIONALITY
+    Object.defineProperty(action, 'name', {value: `${NS}.${action.name}`});
 
-        /**
-         * @see TeqFw_Core_App_Cli_Command.create
-         * @returns {Promise<TeqFw_Core_App_Cli_Command>}
-         */
-        this.create = async function () {
-            // this is sample code:
-            const result = new Command();
-            result.ns = DEF.BACK_REALM;
-            result.name = 'stop';
-            result.desc = 'Stop the HTTP/2 server.';
-            result.action = async function () {
-                try {
-                    const pidPath = $path.join(bootCfg.root, DEF.PID_FILE_NAME);
-                    const data = $fs.readFileSync(pidPath);
-                    const pid = data.toString();
-                    console.info(`Stop web server (PID: ${pid}).`);
-                    process.kill(pid, 'SIGINT');
-                } catch (e) {
-                    console.error('Cannot kill API server process.');
-                }
-            };
-            return result;
-        };
-    }
+    // COMPOSE RESULT
+    const result = new Command();
+    result.ns = DEF.BACK_REALM;
+    result.name = 'stop';
+    result.desc = 'Stop the HTTP/2 server.';
+    result.action = action;
+    return result;
 }
+
+// MODULE'S FUNCTIONALITY
+Object.defineProperty(Factory, 'name', {value: `${NS}.${Factory.name}`});
+
+// MODULE'S EXPORT
+export default Factory;
