3f0d7ed77c3f023300a1d45017fcb48ef06fab07
Alex Gusev <alex@flancer64.com>
2021-04-21 13:06:26 +0300

    CAM-38 Clean up sessions for the same user

--- src/Back/Util.mjs
+++ src/Back/Util.mjs
@@ -4,6 +4,21 @@
  */
 
 /**
+ * Compose expired HTTP cookie to remove existing cookie with the same name in the browser.
+ *
+ * @param {String} name
+ * @param {String} realm
+ * @returns {String}
+ * @memberOf TeqFw_Http2_Back_Util
+ */
+function cookieClear({name, realm = ''}) {
+    // MAIN FUNCTIONALITY
+    const exp = 'Expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    const path = `Path=/${realm}`;
+    return `${name}=; ${exp}; ${path}`;
+}
+
+/**
  * Construct string for 'Set-Cookie' HTTP header.
  *
  * @param {String} name
@@ -17,7 +32,7 @@
  * @returns {String}
  * @memberOf TeqFw_Http2_Back_Util
  */
-function createCookie({name, value, path, expires, domain, secure, httpOnly, sameSite}) {
+function cookieCreate({name, value, path, expires, domain, secure, httpOnly, sameSite}) {
     let result = `${name}=${value};`;
     if (expires) {
         if (expires instanceof Date) {
@@ -40,4 +55,7 @@ function createCookie({name, value, path, expires, domain, secure, httpOnly, sam
     return result;
 }
 
-export {createCookie};
+export {
+    cookieClear,
+    cookieCreate
+};
